395419c0dcbd875860d5ec1766edb445daddf87e
nbhattar <nbhattar@ramapo.edu>
2020-11-04 15:35:05 -0500

    testV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { appDirective } from './app.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, appDirective
   ],
   imports: [
     BrowserModule,
